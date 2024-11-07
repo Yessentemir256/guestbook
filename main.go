18c1daf6b60dd50b603ea5482f7bdb1b7713891d
Yessentemir <yessentemir.utepov@gmail.com>
2024-11-07 23:45:20 +0500

    getStrings added

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Guestbook struct {
@@ -17,10 +19,31 @@ func check(err error) {
 	}
 }
 
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
 	html, err := template.ParseFiles("view.html") // take a file, create a template and parse the content from this file
 	check(err)
-	err = html.Execute(writer, nil) // write in writer the executed template
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook) // write in writer the executed template
 	check(err)
 }
 
